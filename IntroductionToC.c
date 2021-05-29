0579cc9c0a3a4ed96ce639459c43100880587f41
kowshik-407 <aitha.kowshik3@gmail.com>
2021-05-29 20:33:44 +0530

    first commit

---
+++ IntroductionToC.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    printf("Hello World!\n");
+    int age;
+    printf("Enter Age ::> ");
+    scanf("%d",&age);
+    printf("age is %d",age);
+    return 0;
+} // Ctrl + Alt + N
